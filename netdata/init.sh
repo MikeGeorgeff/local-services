288a3635a9abd4a16770b4f0046e6aa6ca457d64
Mike Georgeff <mike.georgeff@uscca.com>
2021-02-07 13:49:09 -0600

    update netdata config location

--- netdata/init.sh
+++ netdata/init.sh
@@ -3,8 +3,7 @@
 source ./environment
 
 mkdir -p $NETDATA_DATA
-mkdir -p $NETDATA_DATA/config
 
 docker run -d --name netdata_temp netdata/netdata
-docker cp netdata_temp:/etc/netdata $NETDATA_DATA/config/
+docker cp netdata_temp:/etc/netdata $NETDATA_DATA/
 docker rm -f netdata_temp
